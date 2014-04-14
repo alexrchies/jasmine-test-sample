85dfb7003b89ec50cbd18d3a3f0bc753b248abf8
alexrchies <alexrchies@gmail.com>
2014-04-14 19:48:51 +0200

    Initial import
    Initial import

---
+++ tests/spec/helloWorldSpec.js
@@ -0,0 +1,9 @@
+describe( "Functions", function () {
+  it("Hello World?", function () {
+	  expect(helloWorld()).toEqual("Hello World");
+  });
+
+  it("Hello World?", function () {
+	  expect(helloWorld()).toEqual("Bye bye World");
+  });
+});
